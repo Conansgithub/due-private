5dd077d82e86072b0f4942b13aa18ba75d6f29db
Conansgithub <1902104816@qq.com>
2025-12-10 14:05:53 +0800

    fix: 更新模块引用为github.com/Conansgithub/due/v2

--- cache/cache.go
+++ cache/cache.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"time"
 
-	"github.com/dobyte/due/v2/errors"
-	"github.com/dobyte/due/v2/log"
+	"github.com/Conansgithub/due/v2/errors"
+	"github.com/Conansgithub/due/v2/log"
 )
 
 var globalCache Cache
